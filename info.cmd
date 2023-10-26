c04c215ebb4e8e0c555d7101a5fbdff7dbeff76a
Neto Evtek <jsn.ads@gmail.com>
2023-10-26 17:04:14 -0300

    SQL INFO

---
+++ info.cmd
@@ -0,0 +1,14 @@
+Inteiro
+    TINYINT     numero pequeno apenas 1 caracter
+    INT         numero de ate 11 caracters
+    BIGINT      numeros gigantes
+REAL
+    FLOAT       numeros decimais
+TEXTO
+    VARCHAR     texto com ate 255 caracters
+    TEXT        texto grandes 
+TEMPO
+    DATE        YYYY-MM-DD
+    TIME        HH:MM:SS
+    DATETIME    YYYY-MM-DD HH:MM:SS
+    TIMESTAMP   converte data em segundos , aparti de 1970-01-01
